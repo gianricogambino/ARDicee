8bff92df8944a3ce7ba820d2acab21d15335c87f
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-13 11:42:46 +0100

    versione finale con codice sporco

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -11,23 +11,37 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    var diceArray = [SCNNode]()
 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         // Set the view's delegate
         sceneView.delegate = self
         
+        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let sphere = SCNSphere(radius: 0.25)
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+//        sphere.materials = [material]
+//
+//        let node = SCNNode()
+//        node.position = SCNVector3(x:0, y:0.1, z:-0.5)
+//
+//        node.geometry = sphere
+//
+//        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+        
+        
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +49,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,34 +62,98 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    // metodo lanciato in automatico quando diciamo che la configuration deve avere il plane detection
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            print("identificato piano orizzontale")
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            //definiamo il piano e stimo a tttenti che va definito per x e z NON y
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x:planeAnchor.center.x, y:0, z:planeAnchor.center.z)
+            //il piano viene considerato cmq in vertticale va trasformato in orizzontale
+            //Float.pi/2 sono 90° in radianti, 1,0,0 invece significa che lo facciamo ruotare attorno all'asse x
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named:"art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+     //dopo aver individuato un piano rileviamo un touch da parte dell'utente
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //se rileva un tocco lo asegna alla sceneView
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            //per convertire il tocco in un punto 3d dobbiamo usare ARkit
+            //qui controlla se il tap è eseguito sul piano che ha individuato
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+                print(hitResult)
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
         
+                // Set the scene to the view
+        //        sceneView.scene = scene
+        
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    // qui andiamo a prendere quella posizione che viene data dalla proprietà worldTransform
+                    // sull'asse y i dadi verrebbero piantati a metà del piano quindi gli sommiamo la loro metà a y per alzarli
+                    // per fare questo usiamo la proprietà boundingSphere.radius
+                    diceNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius, z: hitResult.worldTransform.columns.3.z)
+                    
+                    //aggiungo il dado all'array di dadi
+                    diceArray.append(diceNode)
+                    
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    roll(dice: diceNode)
+                }
+            }
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice:dice)
+            }
+        }
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func roll(dice:SCNNode) {
         
+        //per ruotare i dadi devo ottenere due numeri casuali sull'asse x e z
+        //4 +1 perché devo avere 4 facce vedibili * pi greco mezzi cioè 90°
+        let randX = Float (arc4random_uniform(4) + 1) * (Float.pi/2)
+        let randZ = Float (arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        dice.runAction(SCNAction.rotateBy(x: CGFloat(randX * 3), y: 0, z: CGFloat(randZ * 3), duration: 0.5))
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                dice.removeFromParentNode()
+            }
+        }
     }
 }
