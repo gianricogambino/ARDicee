560b470c518a8e3983698cfc6841fe33cefb7a01
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-13 11:46:04 +0100

    eliminate righe commentate che costruivano una sfera

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -23,26 +23,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-//        let sphere = SCNSphere(radius: 0.25)
-//        let material = SCNMaterial()
-//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
-//        sphere.materials = [material]
-//
-//        let node = SCNNode()
-//        node.position = SCNVector3(x:0, y:0.1, z:-0.5)
-//
-//        node.geometry = sphere
-//
-//        sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
-        
-        
-        // Show statistics such as fps and timing information
-        //sceneView.showsStatistics = true
-        
-
-    }
+        }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
